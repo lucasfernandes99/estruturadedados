be971380080e7544f9e9d8e193cde2f741b98c09
lucasfernandes99 <lucasdm99@outlook.com>
2023-06-28 21:00:59 +0044

    pratica

--- aula2.c
+++ aula2.c
@@ -5,6 +5,8 @@ int main(void){
     float var=2.5;
     float *ponteiro;
     ponteiro=&var;
-    printf("Var= %f \n Ponteiro= %p", var, ponteiro);
+    printf("Var= %f \n Ponteiro= %p\n", var, ponteiro);
+    *ponteiro = 3.7;
+    printf("Var= %f \n Ponteiro= %p\n", var, ponteiro);
     return 0;
 }
