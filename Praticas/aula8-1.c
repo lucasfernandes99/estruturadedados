df4bf0508401dd84896eef33231e26f0ffeb9a30
Lucas Fernandes <138056463+lucasfernandes99@users.noreply.github.com>
2023-07-12 16:47:36 -0300

    12/07/2023

---
+++ Praticas/aula8-1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct disciplina{
+    char nome[30];
+    float nota;
+}Disciplina;
+
+
+typedef struct aluno{
+    char nome[20];
+    int matricula;
+    Disciplina * disciplina;
+}Aluno;
+
+int main(void){
+    
+    Aluno * aluno = (Aluno *) malloc(sizeof(Aluno));
+    if(aluno==NULL){
+        printf("Sem memoria\n");
+        exit(1);
+    }
+    aluno->disciplina = (Disciplina*) malloc(sizeof(Disciplina));
+    if(aluno->disciplina==NULL){
+        printf("Sem memoria\n");
+        exit(1);
+    }
+
+    printf("Digite o nome: \n");
+    scanf(" %[^\n]",aluno->nome);
+
+    printf("Digite a matricula: \n");
+    scanf("%d",&aluno->matricula);
+
+    printf("Digite o nome da disciplina: \n");
+    scanf(" %[^\n]",aluno->disciplina->nome);
+
+    printf("Digite a nota da disciplina: \n");
+    scanf("%f",&aluno->disciplina->nota);
+
+    printf("Os dados informados:\n Nome: %s \t Matricula: %d \t Disciplina: %s \t Nota: %.2f", aluno->nome,aluno->matricula,aluno->disciplina->nome,aluno->disciplina->nota);       
+
+    free(aluno->disciplina);
+    free(aluno);
+    
+    return 0;
+}
