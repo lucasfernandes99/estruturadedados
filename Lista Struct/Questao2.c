7a779e393698be2164aa1136824d2e996ea21897
[lucasfernandes99] <lucasdm99@outlook.com>
2023-07-20 21:23:00 -0300

    20/07/23

---
+++ Lista Struct/Questao2.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct pessoa{
+    char nome[20];
+    int doc;
+    int idade;
+    
+}Pessoa;
+
+void cadastraPessoa(Pessoa * pess){
+    
+    printf("\nDigite o nome: \n");
+    scanf(" %[^\n]",pess->nome);
+
+    printf("Digite o numero do documento: \n");
+    scanf("%d",&pess->doc);
+
+    printf("Digite a idade: \n");
+    scanf("%d",&pess->idade);
+
+}
+
+void imprime(Pessoa *pess){
+    printf("\nDados Informados:\nNome:%s \t Num Documento: %d \t Idade: %d", pess->nome, pess->doc, pess->idade);
+}
+
+void alteraPessoa(Pessoa *pess) {
+    char n;
+    int i;
+    
+    do{
+        printf("\nDeseja alterar a idade s/n: ");
+        scanf("%s",&n);
+        printf("De qual pessoa deseja alterar? \n");
+        scanf("%d",&i-1);
+        printf("Digite a nova idade: ");
+        scanf("%d",&pess[i].idade);
+       
+    }while (n=='s');
+}
+
+/*void maiorMenorSalario(pessionario *pess, int tam) {
+    int i, maior = 0, menor = 0;
+    for (i = 0; i < tam; i++) {
+        if (pess[i].salario > pess[maior].salario) {
+            maior = i;
+        }
+        if (pess[i].salario < pess[menor].salario) {
+            menor = i;
+        }
+        
+    }
+    printf("\nCargo com maior salario: %s \t Salario: R$ %.2f\nCargo com menor salario: %s \t Salario: R$ %.2f", pess[maior].cargo, pess[maior].salario, pess[menor].cargo, pess[menor].salario);
+}*/
+
+int main(void){
+
+    int quant;
+    printf("Digite a quantidade de pessoas: ");
+    scanf("%d", &quant);
+
+    Pessoa pess[quant];
+    int i;
+    for (i = 0; i < quant; i++) {
+        cadastraPessoa(&pess[i]);
+    }
+    for (i = 0; i < quant; i++) {
+        imprime(&pess[i]);
+    }
+    alteraPessoa(&pess[i]);
+    for (i = 0; i < quant; i++) {
+        imprime(&pess[i]);
+    }
+
+    //maiorMenorSalario(pess, quant);
+
+    return 0;
+}
