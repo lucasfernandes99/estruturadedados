9a17683f6f2afbfc2bad86de9fb4595987ffce4c
[lucasfernandes99] <lucasdm99@outlook.com>
2023-07-27 14:32:43 -0300

    Update

--- Lista Struct/Questao1.c
+++ Lista Struct/Questao1.c
@@ -1,75 +1,88 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct funcionario{
-    char nome[20];
+    char nome[50];
     float salario;
     int identificador;
-    char cargo[20];
+    char cargo[50];
 }Funcionario;
 
-void cadastra_funcionario(Funcionario * func){
-    
-    printf("\nDigite o nome: \n");
-    scanf(" %[^\n]",func->nome);
-
-    printf("Digite o salario: \n");
-    scanf("%f",&func->salario);
-
-    printf("Digite o identificador: \n");
-    scanf("%d",&func->identificador);
-
-    printf("Digite o cargo: \n");
-    scanf(" %[^\n]",func->cargo);
-
-}
-
-void imprime(Funcionario *func){
-    printf("\nDados Informados:\nNome:%s \t Salario: R$ %.2f \t Identificador: %d \t Cargo: %s", func->nome, func->salario, func->identificador, func->cargo);
-}
-
-void alteraSalario(Funcionario *func, int quant) {
+void cadastraFuncionario(Funcionario **func, int quant){
     int i;
-    
-    for(i = 0; i < quant; i++) {
-        printf("\nInfome o novo salario do funcionario %d: ", i+1);
-        scanf("%f", &func[i].salario);
-        printf("%f",func[i].salario);
+    for(i=0;i<quant;i++){
+        
+        printf("\nDigite o nome:\n");
+        scanf(" %[^\n]",func[i]->nome);
+
+        printf("Digite o salario:\n");
+        scanf("%f",&func[i]->salario);
+
+        printf("Digite o identificador:\n");
+        scanf("%d",&func[i]->identificador);
+
+        printf("Digite o cargo:\n");
+        scanf(" %[^\n]",func[i]->cargo);
     }
 }
 
+void imprime(Funcionario **func,int quant){
+    int i;
+    printf("\nDados Informados:\n");
+    for(i=0;i<quant;i++){
+        printf("\nFuncionario %d\n", i+1);
+        printf("Nome:%s \t Salario: R$ %.2f \t Identificador: %d \t Cargo: %s\n", func[i]->nome, func[i]->salario, func[i]->identificador, func[i]->cargo);
+    }
 
-void maiorMenorSalario(Funcionario *func, int tam) {
-    int i, maior = 0, menor = 0;
-    for (i = 0; i < tam; i++) {
-        if (func[i].salario > func[maior].salario) {
+}
+
+void alteraSalario(Funcionario ** func,int quant){
+    int i;
+    printf("\n");
+    for(i=0;i<quant;i++){
+        printf("Digite o novo salario do funcionario %d: ",i+1);
+        scanf("%f",&func[i]->salario);
+    }
+}
+
+void maiorMenorSalario(Funcionario ** func, int quant){
+    int i, maior=0, menor=0;
+    for (i=0;i<quant;i++){
+        if (func[i]->salario > func[maior]->salario) {
             maior = i;
         }
-        if (func[i].salario < func[menor].salario) {
+        if (func[i]->salario < func[menor]->salario) {
             menor = i;
         }
-        
     }
-    printf("\nCargo com maior salario: %s \t Salario: R$ %.2f\nCargo com menor salario: %s \t Salario: R$ %.2f", func[maior].cargo, func[maior].salario, func[menor].cargo, func[menor].salario);
-}
+    printf("\nCargo com maior salario: %s \t Salario: R$ %.2f\nCargo com menor salario: %s \t Salario: R$ %.2f", func[maior]->cargo, func[maior]->salario, func[menor]->cargo, func[menor]->salario);
+}   
 
-int main(void){
-
-    int quant;
+int main(){
+    int quant,i;
     printf("Digite a quantidade de funcionarios: ");
     scanf("%d", &quant);
+    Funcionario **func=(Funcionario**) malloc(quant*sizeof(Funcionario*));
 
-    Funcionario func[quant];
-    int i;
-    for (i = 0; i < quant; i++) {
-        cadastra_funcionario(&func[i]);
+    if(func==NULL) {
+        printf("Memoria nao alocada");
+        exit(1);
     }
-    for (i = 0; i < quant; i++) {
-        imprime(&func[i]);
-    }
-    alteraSalario(&func[i], quant);
 
-    maiorMenorSalario(func, quant);
+    for(i=0;i<quant;i++){
+        func[i]=(Funcionario*) malloc(quant*sizeof(Funcionario));
+    }
+
+    cadastraFuncionario(func,quant);
+    imprime(func,quant);
+    alteraSalario(func,quant);
+    maiorMenorSalario(func,quant);
+    
+    for(i=0;i<quant; i++){
+        free(func[i]);
+    }  
+
+    free(func);
 
     return 0;
 }
