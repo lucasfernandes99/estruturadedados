e71c1df34745e6b821b3046d9412a3db0210ac8e
[lucasfernandes99] <lucasdm99@outlook.com>
2023-08-03 17:43:52 -0300

    03/08/2023

--- Unidade 2/Lista Arquivos/Questao2.c
+++ Unidade 2/Lista Arquivos/Questao2.c
@@ -1,41 +1,44 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(void){
-    int *v[10];
-    int i,c,elemento;
-    int menor=0,maior=0;
-    float media;
 
-    FILE * e = fopen("entrada_q2.txt","r");
-    if(e == NULL){
+    char linha[100];
+    int v[10];
+    FILE *e, *s;
+    int i=0;
+    float media=0;
+    
+    e = fopen("entrada_q2.txt","r");
+    if (e==NULL) {
         printf("Erro na abertura do arquivo de entrada!\n");
-        return 1;
+        exit(1);
     }
-    FILE * s = fopen("saida_q2.txt","wt");
-    if(s == NULL){
+
+    s = fopen("saida_q2.txt","w");
+    if (s==NULL) {
         printf("Erro na abertura do arquivo de saida!\n");
         fclose(e);
-        return 1;
+        exit(1);
     }
 
-    while((c = fgetc(e))!=EOF){
-        if(c=='\n'){
-            for(i=0;i<10;i++){
-                fscanf(e,"%d", &v[i]);
-                elemento=elemento+*v[i];
-                if (v[i] > v[maior]){
-                    maior=i;
-                }
-                if (v[i] < v[menor]){
-                    menor=i;
-                }
-            }
-        }
+    while(fgets(linha,100,e)!=NULL){
+        sscanf(linha, "%d", &v[i]);
+        media+=v[i];
+        i++;
     }
 
-    media=elemento/10;
-    fprintf(s,"Menor Elemento: %d\nMaior Elemento: %d\n Media dos Elementos: %f", v[menor],v[maior],media);
+    int menor = v[0];
+    int maior = v[0];
+
+    for(i=0; i<10; i++){
+        if(v[i] < menor)
+            menor =  v[i];
+        if(v[i] > maior)
+            maior =  v[i];
+    }
+
+    fprintf(s,"Menor Elemento: %d\nMaior Elemento: %d\nMedia dos Elementos: %f", menor, maior,media/10);
     
     fclose(e);
     fclose(s);
