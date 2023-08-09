32817612756f7607c8f2d70480a8c54fb6f88061
Lucas Fernandes <138056463+lucasfernandes99@users.noreply.github.com>
2023-08-09 17:28:45 -0300

    09/08/2023

---
+++ Unidade 2/Aula 09.08.23/str.h
@@ -0,0 +1,16 @@
+/* Funçoes oferecidas pelo modulo str.c */
+
+/*Funçao comprimento
+**Retorna o numero de caracteres da string passada como parametro
+*/
+int comprimento (char* str);
+
+/*Funçao copia
+**Copia os caracteres da string orig (origem) para dest (destino)
+*/
+void copia (char* dest, char* orig);
+
+/*Funçao concatena
+**Concatena a string orig (origem) na string dest (destino)
+*/
+void concatena (char* dest, char* orig);
