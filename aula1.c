aac84f8c7d84af2c62b3ceba63ce551932d97cd6
Lucas Fernandes <138056463+lucasfernandes99@users.noreply.github.com>
2024-02-28 20:08:49 -0300

    Update aula1.c

--- aula1.c
+++ aula1.c
@@ -38,26 +38,31 @@ int buscaLinear(aluno v[], int n, int mat){
 int main(){
     int n;
     int i;
-    printf("Informe o valor de N:\n");
+    printf("Informe a quantidade de alunos:\n");
     scanf("%d",&n);
     aluno v[n];
     for (i=0;i<n;i++){
-        printf("Informe os dados:\n");
-        scanf("%d %s %f", &v[i].mat, v[i].nome, &v[i].media);
+        printf("\nInforme os dados do aluno %d:\n",i+1);
+        printf("Matricula: ");
+        scanf("%d", &v[i].mat);
+        printf("Nome: ");
+        scanf("%s", v[i].nome);
+        printf("Media: ");
+        scanf("%f", &v[i].media);
     }
     for (i=0;i<n;i++){
-        printf("Aluno %d:\n",i+1);
+        printf("\nAluno %d:\n",i+1);
         printf("Matricula:%d \t Nome:%s \t Media:%.2f\n", v[i].mat, v[i].nome, v[i].media);
     }
-    printf("Informe a matricula:\n");
+    printf("\nInforme a matricula:\n");
     int mat;
     scanf("%d", &mat);
     int r = buscaBinaria(v,n,mat);
     if (r != -1){
-        printf("Aluno encontrado: %s\n", v[r].nome);
+        printf("\nAluno encontrado: %s\n", v[r].nome);
     }
     else {
-        printf("Sorry. Aluno nao encontrado\n");
+        printf("\nSorry. Aluno nao encontrado\n");
     }
 
 
